33e4cf5ab403c95e432641d7b0e89a988ea8b614
zukeyama <k.zukeyama@on-sight.ne.jp>
2021-12-25 09:56:51 +0900

    add file

---
+++ next-vercel/src/pages/index.styles.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react'
+
+export const container = css`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
---
+++ next-vercel/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import * as styles from './index.styles'
+
+const Home: NextPage = () => {
+  return (
+    <div css={styles.container}>
+      テスト
+    </div>
+  )
+}
+
+export default Home
