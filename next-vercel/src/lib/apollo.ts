2c38b502a9b7b4330f723fefb3d753232baae79d
zukeyama <k.zukeyama@on-sight.ne.jp>
2022-01-04 10:15:07 +0900

    add file

--- next-vercel/src/lib/apollo.ts
+++ next-vercel/src/lib/apollo.ts
@@ -24,7 +24,7 @@ export const cache = new InMemoryCache({
 
 function createApolloClient() {
   const link = createHttpLink({
-    uri: '/api/graphql'
+    uri: 'https://next-vercel-zukeyama-k.vercel.app/api/graphql'
   });
 
   return new ApolloClient({
