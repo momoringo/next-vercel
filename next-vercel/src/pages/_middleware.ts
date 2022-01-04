fc350de80a1df153c0983cad874e752c54c7714e
zukeyama <k.zukeyama@on-sight.ne.jp>
2022-01-04 12:51:22 +0900

    add file

--- next-vercel/src/pages/_middleware.ts
+++ next-vercel/src/pages/_middleware.ts
@@ -1,11 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const middleware = (req: NextRequest) => {
+  const basicAuth = req.headers.get('authorization')
+  //HeaderにAuthorizationが定義されているかをチェック
+  if (basicAuth) {
+    const auth = basicAuth.split(' ')[1]
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
     // basic認証のUser/Passが、envファイルにある値と同じかをチェック
+    if (user ===  process.env.NEXT_PUBLIC_USER && pwd === process.env.NEXT_PUBLIC_PASS) {
+      return NextResponse.next()
+    }
+  }
+
+  const ip = req.headers.get('x-forwarded-for')
 
 
   // 同じでなければエラーを返す
-  return new Response('testes', {
+  return new Response(ip, {
     status: 401
   })
 }
