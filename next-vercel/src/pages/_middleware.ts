c0f225d6bbbf4684a464c29ed54a5269f26d0459
zukeyama <k.zukeyama@on-sight.ne.jp>
2022-01-04 13:15:12 +0900

    add file

--- next-vercel/src/pages/_middleware.ts
+++ next-vercel/src/pages/_middleware.ts
@@ -3,13 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export const middleware = (req: NextRequest) => {
 
 
-  const ip = req.headers.get('x-forwarded-for')
-  if (ip && ip === process.env.IP) {
-    return NextResponse.next()
-  }
 
-  // 同じでなければエラーを返す
-  return new Response(ip, {
-    status: 401
-  })
+
+    return NextResponse.next()
 }
