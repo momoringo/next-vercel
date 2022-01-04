891856ccbe150f0b41bd7ae1428f874a40fa3e7d
zukeyama <k.zukeyama@on-sight.ne.jp>
2022-01-04 09:00:09 +0900

    add file

--- next-vercel/src/pages/index.tsx
+++ next-vercel/src/pages/index.tsx
@@ -2,8 +2,7 @@ import type { NextPage } from 'next'
 import React, { useEffect } from "react";
 import Head from 'next/head'
 import Image from 'next/image'
-import { ThemeProvider } from "@mui/material/styles";
-import { createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { red } from "@mui/material/colors";
 import Editor from '@/components/Editor'
 import { useGetProductsQuery, GetProductsDocument } from '@/graphql'
